cb8ebaad587202944c076c945e519a90fa111a2f
raraujo91 <45054012+raraujo91@users.noreply.github.com>
2019-01-14 00:17:00 -0200

    Add files via upload

--- background.js
+++ background.js
@@ -1,34 +1,71 @@
+function onClickHandler(info, tab) {
+	switch(info.menuItemId){
+		case 'user':
+		chrome.storage.local.get(['user'], function(result) {
+			sendText(result.user);
+		});
+		break;
+
+		case 'pwd':
+		chrome.storage.local.get(['pwd'], function(result) {
+			sendText(result.pwd);
+		});
+		break;
+
+		case 'sign':
+		chrome.storage.local.get(['sign'], function(result) {
+			sendText(result.sign);
+		});
+		break;
+	}
+}
+
+function sendText(cred){
+	chrome.tabs.executeScript({
+		code: 'var temp = document.activeElement.innerHTML; document.activeElement.innerHTML=temp + "' + cred + '";'
+	});
+}
+
+function cleanData() {
+	chrome.storage.local.remove(['user', 'pwd', 'sign'], () => {
+ 		var error = chrome.runtime.lastError;
+    	if (error) {
+        	console.error(error);
+    	}  	
+	});
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-
 	chrome.tabs.executeScript(null, {file: "content.js"});
-
 });
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 chrome.contextMenus.create({
 	title: "Credenciais PayPal",
 	id: "menu", 
-	contexts: ["page"]
+	contexts: ["editable"]
 });
 
 chrome.contextMenus.create({
 	title: "Usuário da API",
 	id: "user",
 	parentId: "menu",
-	contexts: ["page"]
+	contexts: ["editable"]
 });
 
 chrome.contextMenus.create({
 	title: "Senha da API",
 	id: "pwd",
 	parentId: "menu",
-	contexts: ["page"]
+	contexts: ["editable"]
 });
 
 chrome.contextMenus.create({
 	title: "Assinatura da API",
 	id: "sign",
 	parentId: "menu",
-	contexts: ["page"]
+	contexts: ["editable"]
 });
 
 chrome.contextMenus.create({
@@ -37,13 +74,3 @@ chrome.contextMenus.create({
 	contexts: ["browser_action"],
 	onclick: cleanData
 });
-
-function cleanData() {
-	chrome.storage.local.remove(['user', 'pwd', 'sign'], () => {
- 		var error = chrome.runtime.lastError;
-    	if (error) {
-        	console.error(error);
-    	}  	
-
-	});
-}
